53bf1cbf71328013a30ae7c8bac0255e07715446
Kayla Jones <Kaylalyne424@gmail.com>
2014-07-24 14:45:56 -0500

    added #area to Apartment

--- app/models/apartment.js
+++ app/models/apartment.js
@@ -7,4 +7,12 @@ function Apartment(unit){
 
 }
 
+Apartment.prototype.area = function() {
+  var combinedArea = 0;
+  for (var i = 0; i< this.rooms.length; i++) {
+    combinedArea += this.rooms[i].area();
+  }
+  return combinedArea;
+};
+
 module.exports = Apartment;
--- test/unit/apartment.js
+++ test/unit/apartment.js
@@ -5,6 +5,7 @@
 
 var expect = require('chai').expect;
 var Apartment = require('../../app/models/apartment');
+var Room = require('../../app/models/room');
 
 describe('Apartment', function(){
   describe('constructor', function(){
@@ -14,6 +15,17 @@ describe('Apartment', function(){
       expect(a1).to.be.ok;
       expect(a1.rooms).to.have.length(0);
       expect(a1.renters).to.have.length(0);
+    });
+  });
+  describe('area', function(){
+    it('should return the total area of the apartment', function(){
+      var a1 = new Apartment('a1');
+      var r1 = new Room('bedroom', '10', '12');
+      var r2 = new Room('living room', '12', '12');
+      var r3 = new Room('bathroom', '8', '8');
+      a1.rooms.push(r1, r2, r3);
+
+      expect(a1.area()).to.equal(328);
     });
   });
 
